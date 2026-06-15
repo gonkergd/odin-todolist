a712825d358de5cfbe217f0181529367f666f4df
gonkergd <59105483+gonkergd@users.noreply.github.com>
2026-06-15 09:40:42 +0800

    Added an item creator template for a todo list

--- src/index.js
+++ src/index.js
@@ -5,6 +5,30 @@ const newProject = document.querySelector("form");
 const projectList = document.querySelector(".projects");
 let projects = [];
 
+function todoItemCreator() {
+    const makeTodoItem = document.createElement("form");
+    const fieldset = document.createElement("fieldset");
+    const legend = document.createElement("legend");
+    makeTodoItem.appendChild(fieldset);
+    fieldset.appendChild(legend);
+    legend.textContent = "Create a Todo Item: ";
+    formInput(fieldset, "Item name", "item-name", "text");
+    return makeTodoItem;
+}
+
+function formInput(fieldset, name, htmlName, type) {
+    const label = document.createElement("label");
+    label.htmlFor = htmlName;
+    const input = document.createElement("input");
+    label.textContent = name;
+    input.id = htmlName;
+    input.type = type;
+    input.name = htmlName;
+    input.required = true;
+    fieldset.appendChild(label);
+    fieldset.appendChild(input);
+}
+
 newProject.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -12,9 +36,14 @@ newProject.addEventListener("submit", (e) => {
     const projectTitle = document.createElement("div");
     projectTitle.className = "title";
     projectTitle.textContent = formData.get("title");
+    // const projectTitle2 = document.createElement("div");
+    // projectTitle.className = "title";
+    // projectTitle2.textContent = formData.get("title");
     projectDiv.style.backgroundColor = "rgba(" + Math.random() * 255 + ", " + Math.random() * 255 + ", " + Math.random() * 255 + ", 0.2)";
     let todoList = new TodoList(projectDiv);
     projectList.appendChild(projectDiv);
     projectDiv.appendChild(projectTitle);
+    projectDiv.appendChild(todoItemCreator());
+    // projectDiv.appendChild(projectTitle2);
 })
 
