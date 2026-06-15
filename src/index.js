c359cd4f03b25f214821011e4c20f3e44f801f8f
gonkergd <59105483+gonkergd@users.noreply.github.com>
2026-06-15 12:24:23 +0800

    Added basic todo item creation logic
    And some assets that are unused for now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { TodoItem, TodoList } from './items.js';
 import "./styles.css";
 import { todoItemCreator } from './longAssDOMManipulationFunctions.js';
+import { expand } from "./assets/expand.svg";
+import { collapse } from "./assets/collapse.svg";
 
 const newProject = document.querySelector("form");
 const projectList = document.querySelector(".projects");
@@ -26,6 +28,13 @@ newProject.addEventListener("submit", (e) => {
         const newFormData = new FormData(ev.target);
         addTodoItem(todoList.id, newFormData);
     });
+    const destructionButton = document.createElement("button");
+    destructionButton.textContent = "Forget it!";
+    destructionButton.style.order = Number.MAX_SAFE_INTEGER;
+    destructionButton.addEventListener("click", (ev) => {
+        destructionButton.parentElement.remove();
+    });
+    projectDiv.appendChild(destructionButton);
 });
 
 newProject.requestSubmit();
@@ -45,13 +54,17 @@ function addTodoItem(listID, properties) {
     todoItemDiv.className = "todo-item";
     todoItemDiv.style.backgroundColor = properties[4];
     const todoItemTitle = document.createElement("div");
-    todoItemTitle.className = "todo-item-title";
+    todoItemTitle.className = "todo-item-title editable";
     todoItemTitle.textContent = properties[0];
-    const todoItemInfo = document.createElement("div");
-    todoItemInfo.className = "description";
-    todoItemInfo.textContent = properties[1] + " / Due: " + properties[2];
+    const todoItemDueDate = document.createElement("div");
+    const todoItemDesc = document.createElement("div");
+    todoItemDueDate.className = "description editable";
+    todoItemDesc.className = "description editable";
+    todoItemDesc.textContent = properties[1];
+    todoItemDueDate.textContent = "Due: " + properties[2];
     todoItemDiv.style.order = properties[3];
     todoItemDiv.appendChild(todoItemTitle);
-    todoItemDiv.appendChild(todoItemInfo);
+    todoItemDiv.appendChild(todoItemDueDate);
+    todoItemDiv.appendChild(todoItemDesc);
     todoList.elementReference.appendChild(todoItemDiv);
 }
