d7b7e8ee942c2bb1dc8529862e5c7de3d3c4aaa2
gonkergd <59105483+gonkergd@users.noreply.github.com>
2026-06-15 10:26:20 +0800

    Completed and cleaned up form selections
    Put form selection functions into a seperate file so it doesn't overwhelm me

---
+++ src/longAssDOMManipulationFunctions.js
@@ -0,0 +1,61 @@
+export function todoItemCreator() {
+    const makeTodoItem = document.createElement("form");
+    const fieldset = document.createElement("fieldset");
+    const legend = document.createElement("legend");
+    makeTodoItem.appendChild(fieldset);
+    fieldset.appendChild(legend);
+    legend.textContent = "Create a Todo Item: ";
+    formInput(fieldset, "Item name", "item-name", "text");
+    formInput(fieldset, "Description", "description", "text", false);
+    formInput(fieldset, "Due Date", "dueDate", "date");
+    formInput(fieldset, "Priority", "priority", "number");
+    formInput(fieldset, "Color", "color", "color");
+    formInputSelection(fieldset);
+    const button = document.createElement("button");
+    button.textContent = "Create Element!";
+    button.style.marginLeft = "8px";
+    button.style.marginTop = "8px";
+    fieldset.appendChild(button);
+    return [makeTodoItem, button];
+}
+
+export function formInput(fieldset, name, htmlName, type, required) {
+    const label = document.createElement("label");
+    label.style.marginRight = "8px";
+    label.style.marginLeft = "8px";
+    label.style.marginTop = "8px";
+    label.htmlFor = htmlName;
+    const input = document.createElement("input");
+    label.textContent = name;
+    input.id = htmlName;
+    input.type = type;
+    input.name = htmlName;
+    if (required === undefined) input.required = true;
+    fieldset.appendChild(label);
+    fieldset.appendChild(input);
+}
+
+export function formInputSelection(fieldset) {
+    const selectType = document.createElement("select");
+    selectType.id = "item-select";
+    selectType.name = "item-select";
+    const basic = document.createElement("option");
+    basic.value = "basic";
+    basic.textContent = "Basic";
+    const label = document.createElement("label");
+    label.textContent = "Type";
+    label.style.marginRight = "4px";
+    label.style.marginLeft = "4px";
+    label.htmlFor = "item-select";
+    const checklist = document.createElement("option");
+    checklist.value = "checklist";
+    checklist.textContent = "Checklist";
+    const progress = document.createElement("option");
+    progress.value = "progress";
+    progress.textContent = "Progress";
+    selectType.appendChild(basic);
+    selectType.appendChild(checklist);
+    selectType.appendChild(progress);
+    fieldset.appendChild(label);
+    fieldset.appendChild(selectType);
+}
